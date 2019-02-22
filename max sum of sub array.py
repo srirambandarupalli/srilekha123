98652b0b4660af6fd957c703521d15d7a8c03b92
srirambandarupalli <43085615+srirambandarupalli@users.noreply.github.com>
2019-02-22 11:41:49 +0530

    Create max sum of sub array.py

---
+++ max sum of sub array.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+m=l[0]
+k=[]
+k.append(m)
+for x in range(1,len(l)):
+    m=m+l[x] 
+    k.append(m)
+k.append(l[len(l)-1])
+a=l[len(l)-1]
+for y in range(len(l)-2,-1,-1):
+    a=a+l[y] 
+    k.append(a)
+    print(max(k))
